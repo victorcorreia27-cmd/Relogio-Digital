666e62f6697f04bfd6f4eaf22761fc1a3030a06c
victorcorreia27-cmd <victor.correia27@escola.pr.gov.br>
2026-06-16 12:17:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,11 +7,26 @@
 function carregar(){
     //alert("Bem vindo ao JavaScript")
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementsById('imagem')
+    var img = window.document.getElementById('imagem')
 
-    var data = new Data()
+    var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
 
     msg.innerHTML = `Agora são ${hora}:${minutos}Am`
+
+
+
+    if(hora >= 0 && hora <12){
+         img.src = 'manha.png'
+         document.body.style.background='#caf0f8'
+    }
+    else if (hora >= 12 && hora < 18){
+        img.src = 'tarde.png'
+        document.body.style.background='#faa307'
+    }
+    else{
+        img.src = 'noite.png'
+        document.body.style.background='#03045e'
+    }
 }
